80614dcae9d07003275cd8525fa694054f4ad473
emre nogay <64332775+emrenogay@users.noreply.github.com>
2023-03-18 17:46:51 +0300

    Update change-amphtml.php

--- change-amphtml.php
+++ change-amphtml.php
@@ -3,7 +3,7 @@
  *
  * Plugin Name: Change AMPHTML
  * Description: AMP sayfaları için amphtml etiketi domainini değiştirir. Ücretsiz bir yazılımdır. Güncel sürümünü github sayfamdan edinebilirsiniz.
- * Version:     1.5
+ * Version:     1.6
  * Author:      Emre Nogay
  * Plugin URI:  https://github.com/emrenogay/change-amphtml
  * Author URI:  https://emrenogay.com
